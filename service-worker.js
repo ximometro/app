2163cd600367296319c2e372e36fef4c522b56fa
fragar1070-droid <fragar1070@gmail.com>
2025-12-05 06:42:25 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+// Ximometro Service Worker v1.1
+const CACHE_NAME = 'ximometro-v1.1';
+const urlsToCache = [
+  './',
+  'https://emojicdn.elk.sh/%E2%9A%A1?size=192'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('✅ Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('🗑️ Borrando cache viejo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
